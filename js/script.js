1f68decfca8b629a08498933a3701855867c63a4
Piero <piero.onorati@virgilio.it>
2021-06-04 16:46:55 +0200

    add  algorithm section 1 : form

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var submit = document.getElementById('submit');
+
+submit.addEventListener('click', function(){
+
+    // ---------------  inputs --------------//
+
+    var name = document.getElementById('name').value;
+
+    var km = document.getElementById('km').value;
+
+    var age = document.getElementById('age').value;
+    
+
+    // --------- elaboration data -----------//
+
+    var promotion = 'Nessuna promozione';
+
+    var price = km * 0.21
+
+    if (age == 'minorenne') {
+        price -=  price * 0,2;
+        promotion = 'Sconto minorenne';
+        
+    } else if(age == 'over65'){
+        price -=  price * 0,4;
+        promotion = 'Sconto over-65';
+    }
+
+    var carriage = Math.floor(Math.random() * 9 ) + 1;
+
+    var cpCode = Math.floor(Math.random() * 10000 ) + 90000;
+    
+    // ---------------  outputs --------------//
+
+    document.getElementById('ticketname').innerHTML = name;
+
+    document.getElementById('promotion').innerHTML = promotion;
+
+    document.getElementById('carriage').innerHTML = carriage;
+
+    document.getElementById('cpCode').innerHTML = cpCode;
+
+    document.getElementById('price').innerHTML = price + ' &euro;';
+
+})
